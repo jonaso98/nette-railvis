c6becebe8dd39800f945b723551ab448812c4971
jonathan <jonaso98@gmail.com>
2022-01-12 19:15:04 -0300

    gettin post from databases and posted into homepage

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -5,8 +5,29 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
+use Nette\Application\UI\Form;
 
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+    private Nette\Database\Explorer $database;
+
+	public function __construct(Nette\Database\Explorer $database)
+	{
+		$this->database = $database;
+	}
+    
+    public function renderDefault(): void
+    {
+        $this->template->posts = $this->database
+            ->query('select 
+                        p.id, 
+                        p.id_category, 
+                        p.content, 
+                        p.created_at,
+                        category.name as category_name
+                    from posts as p
+                    left join category on category.id = p.id_category
+                    ');
+    }
 }
