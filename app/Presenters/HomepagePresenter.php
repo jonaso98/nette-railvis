b6f045a4429eed7d1ca7919580826536a62f6ef7
jonathan <jonaso98@gmail.com>
2022-01-13 18:43:41 -0300

    Adding the Search button with pagination working

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -17,35 +17,48 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
 		$this->database = $database;
 	}
     
-    public function renderDefault(int $page = 1): void
+    public function renderDefault(string $postValue = null, int $page = 1): void
     {
-        $postsCount = $this->database
+        if ($postValue) {
+            $postsCount = $this->database
+            ->table('posts')
+            ->where('posts.created_at < ?',  new \DateTime)
+            ->whereOr(['posts.content LIKE ?' => '%'.$postValue.'%', 'category.name LIKE ?' => '%'.$postValue.'%'])
+            ->count();
+            $this->template->postValue = $postValue;
+        } else {
+            $postsCount = $this->database
             ->table('posts')
             ->count();
+            $this->template->postValue = null;
+        }
+
+        
         
 		// We'll make the Paginator instance and set it up
 		$paginator = new Nette\Utils\Paginator;
 		$paginator->setItemCount($postsCount); // total articles count
-		$paginator->setItemsPerPage(5); // items per page
+		$paginator->setItemsPerPage(1); // items per page
 		$paginator->setPage($page); // actual page number
 
-        $this->template->posts = $this->database
-            ->query('select 
-                        p.id, 
-                        p.id_category, 
-                        p.content, 
-                        p.created_at,
-                        category.name as category_name,
-                        region.name as region_name
-                    from posts as p
-                    left join category on category.id = p.id_category
-                    left join region ON region.id = p.id_region
-                    WHERE p.created_at < ?
-                    ORDER BY p.created_at DESC
-                    LIMIT ?
-                    OFFSET ?
-                    ', new \DateTime, $paginator->getLength(), $paginator->getOffset());
-        $this->template->paginator = $paginator;
+        $this->template->post_links = $this->database
+            ->fetchAll('Select pl.* from post_external_links as pl');
+
+        $post = $this->database->table('posts');
+        $post->where('posts.created_at < ?',  new \DateTime);
+
+        if ($postValue) {
+            $post->whereOr(['posts.content LIKE ?' => '%'.$postValue.'%', 'category.name LIKE ?' => '%'.$postValue.'%']);
+        }
+        $post->limit($paginator->getLength(), $paginator->getOffset());
+        $post->order('posts.created_at DESC');
+        $post->select('posts.id, posts.id_category, posts.content, posts.created_at, category.name AS category_name, region.name AS region_name');
+
+    if (!$post) {
+        $this->error('Post not found');
+    }
+    $this->template->posts = $post;
+    $this->template->paginator = $paginator;
     }
 
     public function renderShow(int $postId): void
@@ -53,5 +66,19 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
 		$this->template->post = $this->database
 			->table('posts')
 			->get($postId);
+	}
+    protected function createComponentKlientSearchForm() {
+
+		$form = new Form;
+		$form->addText('search')->setRequired(TRUE)
+        ->setHtmlAttribute('class', 'form-control')
+        ->setHtmlAttribute('placeholder', 'Search');
+		$form->addSubmit('send', 'Search');
+		$form->onSuccess[] = [$this, 'klientSearchFormSucceeded'];
+		return $form;
+	}
+
+	public function klientSearchFormSucceeded(Nette\Application\UI\Form $form) {
+			$this->redirect('Homepage:default', $form->getValues()->search, 1);
 	}
 }
